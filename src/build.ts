99f89066f248243688a414025c4db3bf74773a41
robbiesymonds <robbiesymonds26@gmail.com>
2022-07-11 16:14:53 +0930

    check directory is valid before creating.

--- src/build.ts
+++ src/build.ts
@@ -6,20 +6,36 @@ const build = async (path: string): Promise<boolean> => {
   if (path?.length === 0) return false
 
   console.log("Reading file...")
+  const td = new TextDecoder("utf-8")
+  let data
+
+  // Check that the supplied location is valid.
   try {
-    const data = await Deno.readFile(path)
-    const td = new TextDecoder("utf-8")
+    data = await Deno.readFile(path)
     console.log(td.decode(data))
+  } catch (_) {
+    return false
+  }
 
-    const PATH = resolve(`./${DIR}`, "temp.txt")
-    if (!Deno.readDir(DIR)) await Deno.mkdir(DIR)
+  const PATH = resolve(`./${DIR}`, "temp.txt")
+
+  console.log(PATH)
+
+  // Make the directory if does not exist.
+  try {
+    await Deno.stat(DIR)
+  } catch {
+    await Deno.mkdir(DIR)
+  }
+
+  // Write the new data to the file.
+  try {
     await Deno.writeFile(PATH, data, { create: true })
-
     const new_data = await Deno.readFile(`${DIR}/temp.txt`)
     console.log(td.decode(new_data))
 
     return true
-  } catch (_e) {
+  } catch (_) {
     return false
   }
 }
